604d47df69f50b1bf086d13e5a88c05d7bfaaaed
ryan p <rjp-coder@gmail.com>
2025-03-07 15:02:20 +0000

    minor improvements and example code

--- src/mortgageLogic.ts
+++ src/mortgageLogic.ts
@@ -1,3 +1,11 @@
+
+/**
+ * 
+ * @param principal the mortgage amount borrowed
+ * @param r the multiplier for the monthly interest rate. E.g if your mortgage rate is 12%, this would be 1.01
+ * @param n the number of repayments (your mortgage length in years * 12)
+ * @returns 
+ */
 function calculateMortgate(principal:number,r:number,n:number){
     const tNumerator = principal * r ** n
     const tDenominator = r **n-1
@@ -14,19 +22,19 @@ function calculateMortgate(principal:number,r:number,n:number){
  * @param r the multiplier for the monthly interest rate. E.g if your mortgage rate is 12%, this would be 1.01
  * @param payment the amount to be paid monthly
  */
-function generatePaymentsTable(principal,r,payment){
+function generatePaymentsTable(principal:number,r:number,payment:number){
     const data=[];
     let count=0;
     let remaining = principal
 
-    while(remaining>0 && count<100000){
+    while(remaining>0 && count<10000){
         // let {monthlyCost,r}=m //TODO I don't need this?
         let interest = (r-1)*remaining;
         let nowRemaining = remaining + interest - payment;
         let report = {remaining,interest,payment,nowRemaining}
         for (let key in report){
             if (report.hasOwnProperty(key)){
-                report[key]=report[key].toFixed(2)
+                report[key]=report[key].toFixed(0)
             }
         }
         data.push(report);
@@ -38,3 +46,10 @@ function generatePaymentsTable(principal,r,payment){
     return data;
 
 }
+
+const principal=170_851.76
+const rate=7.24
+const years = 27
+let n = years*12
+let r = 1+ rate/12/100
+generatePaymentsTable(principal,r,1202)
